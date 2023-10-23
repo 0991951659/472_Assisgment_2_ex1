4117100e5be2ad21c5b86473139dc4cf7a7f1661
0991951659 <palinsupradit@gmail.com>
2023-10-23 11:23:51 +0100

    see if rstudio push just fine

---
+++ times_100_function.R
@@ -0,0 +1,7 @@
+## Assignment 2. Exercise 1 
+
+## function "times_100": takes a dataset as a single argument and times the values by 100 
+## the function returns the values multipled by 100
+
+# test with the iris data 
+data("iris")
