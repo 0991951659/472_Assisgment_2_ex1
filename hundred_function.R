c73ff6867cb314116d7f793e51fc11ea358edccb
0991951659 <palinsupradit@gmail.com>
2023-10-29 14:38:42 +0000

    Introduce hundred function with one argument

--- hundred_function.R
+++ hundred_function.R
@@ -1,9 +1,9 @@
 ## Assignment 2. Exercise 1 
 
-## function "times_100": takes a dataset as a single argument and times the values by 100 
+# Function "hundred": takes a dataset as a single argument and times the values by 100 
 ## the function returns the values multiplied by 100
 
-times_100 <- function(data) {
+hundred <- function(data) {
   # Identify numeric columns
   numeric_cols <- sapply(data, is.numeric)
   
@@ -14,8 +14,10 @@ times_100 <- function(data) {
 }
 
 
-# test with the iris data: a built-in R dataset with four numeric variables containing 
+# Test with the "iris" data: a built-in R dataset with four numeric variables containing 
 # measurements of 4 different attributes for 50 flowers 
 data("iris")
-times_100(iris)
-# function nw returns the transform data where all the numeric columns are mulitpled by 100 
+hundred(iris)
+# function now returns the transform data where all the numeric columns are multiplied by 100 
+
+
